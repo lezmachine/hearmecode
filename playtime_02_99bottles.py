74dbcdf78ed030ca6ce294ca57aff8ac97c4c2a6
Leslie <leslie.mowrer@gmail.com>
2016-05-22 17:32:58 -0400

    Create playtime_02_99bottles.py

---
+++ playtime_02_99bottles.py
@@ -0,0 +1,3 @@
+for bottle in range(98, 0, -1):
+	print "99 bottles of beer on the wall, 99 bottles of beer ..."
+	print "If one of those bottles should happen to fall, {0} bottles of beer on the wall".format(bottle)
